6cfc9cdfdca259612d75789a4f587c6f0a04737e
moltenangel <matthew@gunur.net>
2023-10-11 15:50:22 -0400

    Updated Lessons

--- main.py
+++ main.py
@@ -16,9 +16,21 @@ def main():
     print("Welcome to gunur.")
     loop = 1
     while loop == 1:
+        print(" 0.) gunur logo")
+        print(" 1.) Tutorial: Lesson 1")
+        print(" 2.) Tutorial: Lesson 2")
+        print(" 3.) Tutorial: Lesson 3")
+        print(" 4.) Tutorial: Lesson 4")
+        print(" 5.) Tutorial: Lesson 5")
+        print(" 6.) Tutorial: Lesson 6")
+        print(" 7.) Tutorial: Lesson 7")
+        print(" 8.) Tutorial: Lesson 8")
+        print("98.) Fibonacci Sequence")
+        print("99.) Exit")
+
         while True:
             try:  # Ensure an integer is returned
-                choice = int(show_menu())
+                choice = int(input("Option: "))
                 break
             except ValueError:
                 print("Invalid input. Try again...")
@@ -34,27 +46,20 @@ def main():
         if choice == 4:
             tut.lesson4()
         if choice == 5:
-            tut.lesson4()
-        elif choice == 8:
+            tut.lesson5()
+        if choice == 6:
+            tut.lesson6()
+        if choice == 7:
+            tut.lesson7()
+        if choice == 8:
+            tut.lesson8()
+        elif choice == 98:
             fib.fib()
-        elif choice == 9:
+        elif choice == 99:
             print("Exiting")
             loop = 0
         else:
             print("Invalid input. Try again...")
-
-
-def show_menu():
-    print("0.) gunur logo")
-    print("1.) Tutorial: Lesson 1")
-    print("2.) Tutorial: Lesson 2")
-    print("3.) Tutorial: Lesson 3")
-    print("4.) Tutorial: Lesson 4")
-    print("5.) Tutorial: Lesson 5")
-    print("8.) Fibonacci Sequence")
-    print("9.) Exit")
-    var = input("Option: ")
-    return var
 
 
 main()
--- tutorial.py
+++ tutorial.py
@@ -2,6 +2,9 @@
 # https://www.freecodecamp.org/news/ultimate-beginners-python-course/
 
 import math
+import sys
+import random
+from enum import Enum
 
 
 def lesson1():  # Getting Started
@@ -180,10 +183,139 @@ def lesson4():  # Data Types
     print(round(gpa))  # Round to the nearest integer = 3
     print(round(gpa, 1))  # Round to the nearest n decimal = 3.3
 
+    print(math.pi)
+    print(math.sqrt(64))  # 8
+    print(math.ceil(gpa))  # round up to the nearest integer
+    print(math.floor(gpa))  # round down to the nearest integer
+
+    # CAsting a string into a number
+    zipcode = "29651"
+    zip_value = int(zipcode)
+    print(type(zip_value))
+
+    # Error if you attempt to cast the incorrect data
+    # zip_value = int("New York")  # Invalid literal error
     return
 
 
-def lesson5():  #
+def lesson5():  # User Input
+    # Rock Paper Scissors
+    print("")
+    playerchoice = input('Enter...\n1 for Rock,\n2 for Paper, or\n3 for Scissors:\n\n')
+    player = int(playerchoice)
+    if player < 1 or player > 3:
+        sys.exit("You must enter 1, 2, or 3.")  # Don't forget to import sys
+
+    computerchoice = random.choice("123")
+    computer = int(computerchoice)
+    print("")  # Display numeric values
+    print("You chose " + playerchoice + ".")
+    print("Python chose " + computerchoice + ".")
+    print("")
+
+    class RPS(Enum):
+        ROCK = 1  # Constant variables use all caps
+        PAPER = 2
+        SCISSORS = 3
+
+    print("")  # Display Enum values
+    print("You chose " + str(RPS(player)) + ".")
+    print("Python chose " + str(RPS(computer)) + ".")
+    print("")
+
+    print("")  # Remove the classname from the output
+    print("You chose " + str(RPS(player)).replace('RPS.', '') + ".")
+    print("Python chose " + str(RPS(computer)).replace('RPS.', '') + ".")
+    print("")
+
+    print("")  # Convert to Title case for aesthetics
+    print("You chose " + str(RPS(player)).replace('RPS.', '').title() + ".")
+    print("Python chose " + str(RPS(computer)).replace('RPS.', '').title() + ".")
+    print("")
+
+    # Windows Key + "." = Emoji selection window
+
+    if player == 1 and computer == 3:
+        print("🎉 You win!")
+    elif player == 2 and computer == 1:
+        print("🎉 You win!")
+    elif player == 3 and computer == 2:
+        print("🎉 You win!")
+    elif player == computer:
+        print("😲 Tie game!")
+    else:
+        print("🐍 Python wins!")
+
+    # Introduction to Enums
+    # import command:
+    # from enum import Enum
+    # Included class RPS at beginning of code
+
+    print(RPS(2))  # RPS.PAPER
+    print(RPS.ROCK)  # RPS.ROCK
+    print(RPS['ROCK'])  # RPS.ROCK
+    print(RPS.ROCK.value)  # 1
+    print(str(RPS.ROCK))  # RPS.ROCK
+    print(str(RPS.ROCK).replace('RPS.', ''))  # ROCK
+    print(str(RPS.ROCK).replace('RPS.', '').title())  # Rock
+    return
+
+
+def lesson6():  # Lists & Tuples
+    users = ['Dave', 'John', 'Sara']
+    data = ['Dave', 42, True]
+    emptylist = []
+    print("Dave" in users)  # True
+    print("Dave" in emptylist)  # False
+    print(users[0])  # Dave
+    print(users[-1])  # Sara
+    print(users[-2])  # John
+
+    print(users.index('Sara'))  # 2
+
+    print(users[0:2])  # ['Dave', 'John']
+    print(users[1:])  # ['John', 'Sara']
+    print(users[-3:-1])  # ['Dave', 'John'] (same as [0:2]
+
+    print(len(data))  # 3
+
+    # Adding data to a list
+    # users.append('Elsa')  # Adds Else to the end of the list
+    print(users)  # ['Dave', 'John', 'Sara', 'Elsa']
+    users += ['Jason']  # Adds Jason to the end of the list
+    print(users)  # ['Dave', 'John', 'Sara', 'Elsa', 'Jason']
+    # users += 'Jason'  # (incorrectly adds each letter to the list)
+    # Extend adds items to the end of the list
+    users.extend(['Robert', 'Jimmy'])  # Adds Robert and Jimmy to the end of the list
+    print(users)  # ['Dave', 'John', 'Sara', 'Elsa', 'Jason', 'Robert', 'Jimmy']
+    users.extend(data)  # Adds the data list to the end of the list
+    print(users)  # ['Dave', 'John', 'Sara', 'Elsa', 'Jason', 'Robert', 'Jimmy','Dave', 42, True]
+    # Insert adds items to the start of the list
+    users.insert(0,'Bob')  # Inserts Bob to the beginning of the list
+    print(users)  # ['Bob', 'Dave', 'John', 'Sara', 'Elsa', 'Jason', 'Robert', 'Jimmy']
+
+    # Slicing (example: users[2:2])
+    # users[2:2] = ['Eddie', 'Alex']  # Adds Eddie and Alex starting at the second index
+    print(users)  # ['Bob', 'Dave', 'Eddie', 'Alex', 'John', 'Sara', 'Elsa', 'Jason', 'Robert', 'Jimmy']
+    users[1:3] = ['Robert', 'JPJ']  # Replaced the second and third indexes in the list
+    print(users)  # ['Bob', 'Robert', 'JPJ', 'Alex', 'John', 'Sara', 'Elsa', 'Jason', 'Robert', 'Jimmy']
+
+    # Removing data from a list
+    users.remove('Bob')  # Remove Bob from the list
+    print(users)  # ['Robert', 'JPJ', 'Alex', 'John', 'Sara', 'Elsa', 'Jason', 'Robert', 'Jimmy']
+
+    print(users.pop())  # Removes the last index of the list
+    print(users)  # ['Robert', 'JPJ', 'Alex', 'John', 'Sara', 'Elsa', 'Jason', 'Robert']
+
+    return
+
+
+def lesson7():  # Dictionaries & Sets
+
+    return
+
+
+def lesson8():  # Loops
 
     return
 
@@ -195,4 +327,4 @@ def lesson():  #
 
 if __name__ == '__main__':
     # print('This script is not meant to be ran by itself. Please run main.py.')
-    lesson4()
+    lesson6()
