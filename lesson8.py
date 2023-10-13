329f42690ba7085fbdb6f971d820a34eefa6bae9
moltenangel <matthew@gunur.net>
2023-10-13 17:11:39 -0400

    Rearranged lessons into individual files and updated menu

--- lesson8.py
+++ lesson8.py
@@ -2,6 +2,10 @@
 # https://www.freecodecamp.org/news/ultimate-beginners-python-course/
 # Lesson 8 - Loops
 
+import sys
+import random
+from enum import Enum
+
 
 def lesson8():
     # While Loops
@@ -74,9 +78,59 @@ def lesson8():
     actions = ["codes","eats","sleeps"]
 
     # Nested loop
-    for name in names:
-        for action in actions:
+    for name in names:  # Outside Loop
+        for action in actions:  # Inside Loop
             print(name + " " + action + ".")
+
+    lesson8b()
+    return
+
+
+def lesson8b():  # Modify the code from lesson 5
+    # Rock Paper Scissors
+    class RPS(Enum):
+        ROCK = 1  # Constant variables use all caps
+        PAPER = 2
+        SCISSORS = 3
+
+
+    playagain = True
+
+    while playagain == True:
+        playerchoice = input('\nEnter...\n1 for Rock,\n2 for Paper, or\n3 for Scissors:\n\n')
+        player = int(playerchoice)
+        if player < 1 or player > 3:
+            sys.exit("You must enter 1, 2, or 3.")  # Don't forget to import sys
+
+        computerchoice = random.choice("123")
+        computer = int(computerchoice)
+
+        print("You chose " + str(RPS(player)).replace('RPS.', '').title() + ".")
+        print("Python chose " + str(RPS(computer)).replace('RPS.', '').title() + ".\n")
+
+        # Windows Key + "." = Emoji selection window
+
+        if player == 1 and computer == 3:
+            print("🎉 You win!")
+        elif player == 2 and computer == 1:
+            print("🎉 You win!")
+        elif player == 3 and computer == 2:
+            print("🎉 You win!")
+        elif player == computer:
+            print("😲 Tie game!")
+        else:
+            print("🐍 Python wins!")
+
+        question = input("\nPlay again?\nY for Yes or \nQ to Quit \n\n")
+        print(question)
+        if question.lower() == "y":
+            continue
+        else:
+            print("\n🎉🎉🎉🎉")
+            print("Thank you for playing!\n")
+            playagain = False  # break would also work here
+
+    # sys.exit("Bye! 🙌")
     return
 
 
