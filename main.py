f49711cc94922b47e12a4ae13ba35d052cee06ff
moltenangel <matthew@gunur.net>
2023-11-03 22:24:05 -0400

    Added/Updated files

--- main.py
+++ main.py
@@ -3,10 +3,6 @@
 # https://www.freecodecamp.org/news/ultimate-beginners-python-course/
 
 import fib
-from Lesson13 import f_strings as l13
-from Lesson14 import modules as l14
-from Lesson15 import hello_person as l15
-import lesson16 as l16
 
 
 def gunur_logo():
@@ -19,13 +15,9 @@ def main():
     print("Welcome to gunur.")
     loop = 1
     while loop == 1:
-        print(" 0.) gunur logo")
-        print(" 13.) Tutorial: Lesson 13")
-        print(" 14.) Tutorial: Lesson 14")
-        print(" 15.) Tutorial: Lesson 15")
-        print(" 16.) Tutorial: Lesson 16")
-        print("98.) Fibonacci Sequence")
-        print("99.) Exit")
+        print(" 1.) gunur logo")
+        print("2.) Fibonacci Sequence")
+        print("3.) Exit")
 
         while True:
             try:  # Ensure an integer is returned
@@ -34,19 +26,11 @@ def main():
             except ValueError:
                 print("Invalid input. Try again...")
 
-        if choice == 0:
+        if choice == 1:
             gunur_logo()
-        if choice == 13:
-            l13.lesson13()
-        if choice == 14:
-            l14.lesson14()
-        if choice == 15:
-            l15.lesson15()
-        if choice == 16:
-            l16.lesson16()
-        elif choice == 98:
+        elif choice == 2:
             fib.fib()
-        elif choice == 99:
+        elif choice == 3:
             print("Exiting")
             loop = 0
         else:
